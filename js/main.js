5d1afe84af4cb20203edf2befae412daf79bf480
Mahmoud Ahmed <45967439+MAhmoud2527@users.noreply.github.com>
2020-06-03 14:45:17 +0200

    add files

--- js/main.js
+++ js/main.js
@@ -23,6 +23,22 @@ function changeLang(lang) {
             document.getElementById('lnagLink').setAttribute('href', 'css/style-ar.css');
     }
 }
+// upload User img
+function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      
+      reader.onload = function(e) {
+        $('#blah').attr('src', e.target.result);
+      }
+      
+      reader.readAsDataURL(input.files[0]); // convert to base64 string
+    }
+  }
+$(".imgInp").change(function() {
+    readURL(this);
+  });
+  
 /* Arrow*/
 $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
